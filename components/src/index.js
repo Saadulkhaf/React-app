493f22c96adcb58f88bebd3d8e4f2f37aef1e8dd
saadulkhaf <saadafzal995@gmail.com>
2020-02-09 20:45:40 +0500

    added faker library

--- components/src/index.js
+++ components/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 
 const App = () =>{
@@ -7,11 +8,11 @@ const App = () =>{
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img alt="avatar" src={faker.image.avatar()}/>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
-                        Saad
+                        daniel
                     </a>
                     <div className="metadata">
                         <span className="date">Today at 9:00pm</span>
