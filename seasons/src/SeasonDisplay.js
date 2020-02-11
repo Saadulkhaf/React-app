cc1462eaaf3909c5b4296183c2f0e6b63789b726
saadulkhaf <saadafzal995@gmail.com>
2020-02-11 15:48:09 +0500

    showing icons on the screen via semantic ui

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+const getSeason=(lat, month) => {
+    if(month > 2 && month < 9 ) {
+        return lat > 0 ? "summer" : "winter";
+    }else{
+        return lat > 0 ? "winter" : "summer"
+    }
+}
+
 const SeasonDisplay=(props) => {
-    console.log(props.lat);
+    const season = getSeason(props.lat, new Date().getMonth());
+    const textOnScreen = season === "winter" ? "Oh its chilly outside" : "Lets hit the beach";
+    const icon = season === "winter" ? "snowflake" : "sun";
+    
     return (
-        <div>Season</div>
+    <div>
+        <i className={`${icon} icon`} />
+        <h1>{textOnScreen}</h1>
+        <i className={`${icon} icon`} />
+    </div>
     );
 }
 
