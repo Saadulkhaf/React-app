710baf86862e375b52106e731b08ab1a09a6bd55
saadulkhaf <saadafzal995@gmail.com>
2020-02-11 13:28:39 +0500

    refactored geolocation to lifecycle methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,11 +5,9 @@ import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = {lat: null, errorMessage: ""}
 
-        this.state={lat: null, errorMessage: ""};
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({lat: position.coords.latitude});
@@ -26,7 +24,7 @@ class App extends React.Component {
             return <div>Error: {this.state.errorMessage}</div>
         }
         if(!this.state.errorMessage && this.state.lat){
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
         return <div>Loading....</div>
     
