2c2e786d47d5981fd15caaffe9955ef277029039
saadulkhaf <saadafzal995@gmail.com>
2020-02-14 00:02:20 +0500

    Api request implemented successfully

---
+++ videos/src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyDhMwi_P5bYrFmja4h14MjZjnEMbrfn-yk';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: `${KEY}`,
+        type: 'video'
+    }
+});
--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
     onFormSubmit = (event) => {
         event.preventDefault();
 
-        // to do: make sure to call callback from parent component
+        this.props.onTermSubmit(this.state.term);
     }
 
     render() {
