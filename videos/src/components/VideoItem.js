d744a8935f9084afcd1d14d75944469377554351
saadulkhaf <saadafzal995@gmail.com>
2020-02-14 14:10:49 +0500

    got video title and thumbnail to show on screen

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const VideoItem = (props) => {
     return(
-        <div>Video Item</div>
+        <div>
+            <img src={props.video.snippet.thumbnails.medium.url}/>
+            {props.video.snippet.title}
+        </div>
     );
 }
 
