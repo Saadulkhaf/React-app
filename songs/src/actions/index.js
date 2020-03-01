97a33ed084297baf4bd1990f1e65fead9c0fa5da
saadulkhaf <saadafzal995@gmail.com>
2020-03-01 13:56:58 +0500

    created reducers, imported combineReducers

---
+++ songs/src/actions/index.js
@@ -0,0 +1,8 @@
+// action creater 
+
+export const selectSong = (song) => {
+    return {
+        type: 'SONG_SELECTED',
+        payload: song
+    };
+};
---
+++ songs/src/reducers/index.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux';
+
+// reducers
+
+const songsListReducer = () => {
+    return [
+        {title: 'All of me', duration: '4:05'},
+        {title: 'Love me like you do', duration: '3:12'},
+        {title: 'Swing', duration: '2:30'},
+        {title: 'Are you with me', duration: '3:20'}
+    ];
+};
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if(action.type === 'SONG_SELECTED') {
+        return action.payload;
+    };
+
+    return selectedSong;
+};
+
+export default combineReducers({
+    songs: songsListReducer,
+    selectedSong: selectedSongReducer
+});
