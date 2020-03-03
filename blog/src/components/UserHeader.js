ba4b51a26750382e9c51071c9c0f2a1143350ae7
saadulkhaf <saadafzal995@gmail.com>
2020-03-03 18:19:16 +0500

    extracted logic to mpStateToProp instd of compnent

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -8,7 +8,7 @@ class UserHeader extends React.Component {
         // console.log(this.props.userId);
     }
     render(){
-        const user = this.props.users.find((user) => user.id === this.props.userId);
+        const { user } = this.props;
         if (!user) {
             return <div>waiting...</div>;
         }
@@ -19,8 +19,8 @@ class UserHeader extends React.Component {
     };
 };
 
-const mapStateToProps = (state) => {
-    return {users: state.users};
+const mapStateToProps = (state, ownProps) => {
+    return {user: state.users.find((user) => user.id === ownProps.userId)};
 };
 
 export default connect(mapStateToProps, {fetchUser: fetchUser})(UserHeader);
