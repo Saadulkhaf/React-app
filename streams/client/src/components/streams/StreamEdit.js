2efbddb2f7b9f541f089278af89869040df77a34
saadulkhaf <saadafzal995@gmail.com>
2020-03-20 13:43:52 +0500

    initialValues passed as a prop using pick from _

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
     }
 
+    onSubmit(formValues){
+        console.log(formValues);
+    }
+
     render(){
-    console.log(this.props);
+    // console.log(this.props);
     return (
-        <div>StreamEdit</div>
+        <div>
+            <h3>
+                Edit Stream!
+            </h3>
+            <StreamForm initialValues={_.pick(this.props.stream, 'title', 'description')} onSubmit={this.onSubmit}/>
+        </div>
     );
     }
 }
@@ -19,4 +30,6 @@ const mapStateToProps = (state, ownProps) => {
     return {stream: state.streams[ownProps.match.params.id]}
 }
 
-export default connect(mapStateToProps, { fetchStream: fetchStream })(StreamEdit);
+export default connect(
+    mapStateToProps,
+    { fetchStream: fetchStream, editStream: editStream })(StreamEdit);
