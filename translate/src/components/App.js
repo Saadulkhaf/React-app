54441e22ff6a614c52d3d05d9695dbbaaced9730
saadulkhaf <saadafzal995@gmail.com>
2020-03-26 16:57:18 +0500

    event hsndler onClick

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = {language: 'english'};
+    onLanguageChange = (language) => {
+        this.setState({language: language});
+    }
+
     render() {
         return (
-            <div>App</div>
+            <div className="ui container">
+                <div>
+                    Select a Language:
+                    <i className="flag us" onClick={()=> this.onLanguageChange("english")}/>
+                    <i className="flag pk" onClick={()=> this.onLanguageChange("urdu")}/>
+                </div>
+                {this.state.language}
+            </div>
         )
     };
 };
