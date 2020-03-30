48890b4634b18a26a4123ce77539703956a3a602
saadulkhaf <saadafzal995@gmail.com>
2020-03-30 15:56:08 +0500

    event handelers updating state

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = { resources: 'posts'}
+
     render() {
         return(
-            <div>App</div>
+            <div>
+                <div>
+                    <button onClick={()=> this.setState({resources: 'posts'})}>Posts</button>
+                    <button onClick={()=> this.setState({resources: 'todos'})}>Todos</button>
+                </div>
+                {this.state.resources}
+            </div>
         );
     };
 }
