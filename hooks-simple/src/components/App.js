dab5db743b852380b69808435fdde8633e44fbba
saadulkhaf <saadafzal995@gmail.com>
2020-04-03 18:13:09 +0500

    made network request in comdidmnt but not wrking

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
         <div>
             <div>
                 <button onClick={()=> setResources('posts')}>Posts</button>
-                <button onClick={()=> setResources('todooos')}>Todos</button>
+                <button onClick={()=> setResources('todos')}>Todos</button>
             </div>
             <ResourceList resources = { resources }/>
         </div>
--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 
 class ReasourceList extends React.Component {
+    state = { resources: [] };
+
+    async componentDidMount() {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resources}`);
+
+        this.setState({resources: response.data})
+    }
+
     render() {
         return (
             <div>
-                {this.props.resources}
+                {this.state.resources.length}
             </div>
         );
     };
