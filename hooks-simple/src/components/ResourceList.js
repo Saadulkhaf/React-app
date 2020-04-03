7a6b0c7843f01f69ea3d935f27a1e42ec38ff9ef
saadulkhaf <saadafzal995@gmail.com>
2020-04-03 18:36:06 +0500

    refactoring resourceList as functional comp

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,30 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ReasourceList extends React.Component {
-    state = { resources: [] };
+const ReasourceList = () => {
+    const [resources, setResources ] = useState([]);
 
-    async componentDidMount() {
+    const fetchResources = async () => {
         const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resources}`);
 
-        this.setState({resources: response.data})
+        setResources(response.data);
     }
 
-    async componentDidUpdate(prevProps) {
-        if (prevProps.resources !== this.props.resources) {
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resources}`);
-
-            this.setState({resources: response.data})
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.resources.length}
-            </div>
-        );
-    };
+    return (
+        <div>
+            {resources.length}
+        </div>
+    );
 }
 
 export default ReasourceList;
