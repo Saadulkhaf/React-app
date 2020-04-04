960274d072b51cc098aaa276c0d34cf9d3475728
saadulkhaf <saadafzal995@gmail.com>
2020-04-04 21:51:45 +0500

    reuseable code, func with apicall seperate file

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
-import ResourceList from './ResourceList';
+import React, { useState } from "react";
+import ResourceList from "./ResourceList";
 
 const App = () => {
-    const [resources, setResources] = useState('posts');
+  const [resources, setResources] = useState("posts");
 
-    return(
-        <div>
-            <div>
-                <button onClick={()=> setResources('posts')}>Posts</button>
-                <button onClick={()=> setResources('todos')}>Todos</button>
-            </div>
-            <ResourceList resource = { resources }/>
-        </div>
-    );
-
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResources("posts")}>Posts</button>
+        <button onClick={() => setResources("todos")}>Todos</button>
+      </div>
+      <ResourceList resource={resources} />
+    </div>
+  );
 };
 
 export default App;
--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,35 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from "react";
+import useResources from './useResources';
 
-const useResources = (resource) => {
-    const [resources, setResources ] = useState([]);
-
-    useEffect (() => {
-        (async (resource) => {
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-    
-            setResources(response.data);
-        })(resource)
-    }, [resource])
-
-    return resources;
-}
 
 const ReasourceList = ({ resource }) => {
-    const resources = useResources(resource);
-    
+  const resources = useResources(resource);
 
-    return (
-        <div>
-            <ul>
-                {resources.map(record => {
-                    return (
-                        <li key={record.id}>{record.title}</li>
-                    )
-                })}
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div>
+      <ul>
+        {resources.map((record) => {
+          return <li key={record.id}>{record.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default ReasourceList;
