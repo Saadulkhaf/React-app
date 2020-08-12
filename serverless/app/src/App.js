2bd1770c452da6904417da810eb4fb4caecede8e
saadulkhaf <saadafzal995@gmail.com>
2020-08-12 16:54:23 +0500

    boiler plate setup

--- serverless/app/src/App.js
+++ serverless/app/src/App.js
@@ -1,1 +1,53 @@
 import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    isLoading: false,
+    invoices: [],
+  };
+
+  render() {
+    const isLoading = this.state.isLoading;
+    const invoices = this.state.invoices;
+
+    const body = [
+      {
+        id: "100",
+        vendor: "Henkok",
+        amount: "$18,000",
+        date: "15/05/1995",
+      },
+      {
+        id: "100",
+        vendor: "Henkok",
+        amount: "$18,000",
+        date: "15/05/1995",
+      },
+      {
+        id: "100",
+        vendor: "Henkok",
+        amount: "$18,000",
+        date: "15/05/1995",
+      },
+      {
+        id: "100",
+        vendor: "Henkok",
+        amount: "$18,000",
+        date: "15/05/1995",
+      },
+    ];
+
+    if (isLoading) {
+      return <div>Loading</div>;
+    }
+    return (
+      <div className="container border border-secondary rounded center">
+        <div className="row">
+          <div className="col-12">Invoices pending - Test company</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
