fe76ea34958fa40523cc16ca4b4b12dbce255b7d
saadulkhaf <saadafzal995@gmail.com>
2020-08-12 15:57:41 +0500

    bootstrap and reactstrap added, awesomefort

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,47 +4,47 @@ import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetails from './VideoDetails';
 
-class App extends React.Component{
+class App extends React.Component {
 
-    state = {videos: [], selectedVideo: null};
+    state = { videos: [], selectedVideo: null };
 
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
-            params:{
+            params: {
                 q: term
             }
         })
-        this.setState({videos: response.data.items, selectedVideo: response.data.items[0] });
+        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
         // console.log(term);
     };
 
 
     onVideoSelect = (video) => {
-        this.setState({selectedVideo: video})
+        this.setState({ selectedVideo: video })
         // console.log("From the App", video);
     }
 
     render() {
-        return(
+        return (
             <div className="ui container">
-                <SearchBar onTermSubmit={this.onTermSubmit}/>
+                <SearchBar onTermSubmit={this.onTermSubmit} />
                 <div className="ui grid">
                     <div className="ui row">
                         <div className="eleven wide column">
-                            <VideoDetails selectedVideo={this.state.selectedVideo}/>
+                            <VideoDetails selectedVideo={this.state.selectedVideo} />
                         </div>
                         <div className="five wide column">
-                            <VideoList 
-                            onVideoSelect={this.onVideoSelect} 
-                            videos={this.state.videos}/>
+                            <VideoList
+                                onVideoSelect={this.onVideoSelect}
+                                videos={this.state.videos} />
                         </div>
 
                     </div>
 
                 </div>
-                
-                
-            </div>  
+
+
+            </div>
         );
     }
 }
